bda074112704f42da4f9aa739854b9c314e4c02a
Philip Rosing <philip.v.rosing@gmail.com>
2019-02-05 15:18:23 +0100

    first commit

---
+++ settings.js
@@ -0,0 +1,71 @@
+var settings = {
+	"username":"Voro",
+	"useGreeting":1,
+	"openNewTab":1,
+	"useDateLinkCalendar":0,
+	"calendarLink":"",
+	"accentColor":"#49738E",
+	"mainColor":"white",
+	"backgroundImage":"assets/bg.jpg",
+	"backgroundPosition":"bottom center"
+};
+/*
+	Settings:
+	# username (string)
+		- If you want greeting messages, why not personalize them?
+		- Example: 
+				"username":"John Doe"
+
+	# useGreeting (int)
+		- Defines wether or not to display greetings.
+		- Examples:
+				"useGreeting":0
+				"useGreeting":1
+
+	# openNewTab (int)
+		- Defines if links should be opened in same tab or new one.
+		- Examples:
+				"openNewTab":0
+				"openNewTab":1
+
+	# useDateLinkCalendar (int)
+		- The date below the clock is a link, you can set it to your favorite calender app.
+		- Examples:
+				"useDateLinkCalendar":0
+				"useDateLinkCalendar":1
+
+	# calendarLink (string)
+		- This is only used if useDateLinkCalendar is set to 1.
+		- Example:
+				"calendarLink":"https://calendar.google.com"
+
+	# accentColor (string)
+		- Choose your own colors for hovered links and active search bar.
+		- Any valid css color can be used.
+		- Examples:
+				"accentColor":"#ff0000"
+				"accentColor":"rgb(255,0,0)"
+				"accentColor":"red"
+
+	# mainColor (String)
+		- Choose your own colors for, well everything.
+		- Any valid css color can be used.
+		- Examples:
+				"mainColor":"#fff"
+				"mainColor":"rgb(255,255,255)"
+				"mainColor":"white"
+
+	# backgroundImage (string)
+		- Choose your own background image. Write full path.
+		- So if the image is in the assets folder use "assets/yourImage.jpg".
+		- Examples:
+				"backgroundImage":"assets/background.jpg"
+				"backgroundImage":"background.png"
+
+	# backgroundPosition (string)
+		- Where should the image originate from and alwas show when scaled.
+		- Any valid css background positions can be used.
+		- Examples:
+				"backgroundPosition":"bottom center"
+				"backgroundPosition":"center center"
+*/
